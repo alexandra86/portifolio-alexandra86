13f1b40591e46b37a20fd6b775e3ad9e3d2d2b14
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-03-29 21:20:18 -0300

    feat/contactModal

--- src/components/ContactModal/index.jsx
+++ src/components/ContactModal/index.jsx
@@ -2,6 +2,10 @@ import Modal from "react-modal";
 import { React, useContext } from "react";
 import { PortContext } from "../../contexts/PortContext";
 import { StyledContainerModal } from "./styles";
+import { Link } from "react-router-dom";
+import linkedinLogo from "../../img/linkedin.svg";
+import gitHubLogo from "../../img/github.png";
+import whatsAppLogo from "../../img/whatsapp.svg";
 Modal.setAppElement("#root");
 
 export function ContactModal() {
@@ -14,10 +18,40 @@ export function ContactModal() {
         className="modalContent"
       >
         <div className="headerModal">
-          <h2 className="titleModal">Contato</h2>
           <button onClick={() => handleContactModal()} className="btCloseModal">
             X
           </button>
+        </div>
+        <h2 className="titleModal">Contatos:</h2>
+        <div className="divContact">
+          <Link
+            target="_blank"
+            to="https://www.linkedin.com/in/alexandra-miranda86/"
+            className="linkedin"
+          >
+            <img src={linkedinLogo} alt="Logo do linkedin" />
+          </Link>
+
+          <Link
+            target="_blank"
+            to="https://github.com/alexandra86"
+            className="gitHub"
+          >
+            <img src={gitHubLogo} alt="Logo do github" />
+          </Link>
+
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            to="https://web.whatsapp.com/send?phone=+5521989964940"
+            className="whatsApp"
+          >
+            <img
+              src={whatsAppLogo}
+              alt="Logo do whatsapp"
+              className="whatsAppImage"
+            />
+          </Link>
         </div>
       </div>
     </StyledContainerModal>
--- src/components/ContactModal/styles.js
+++ src/components/ContactModal/styles.js
@@ -12,18 +12,74 @@ export const StyledContainerModal = styled.div`
   justify-content: center;
 
   .modalContent {
-    max-width: 23.0625rem;
+    max-width: 31.25rem;
+    height: 18.75rem;
     width: 100%;
-    border-radius: 0.25rem;
-    background-color: #343b41;
+    border-radius: 0.5rem;
+    background-color: #000000;
+    border: 0.25rem solid #a5b82e;
+    animation: animation 1.5s ease-in;
+    transform: 1.5s ease-in;
   }
   .headerModal {
-    max-width: 23.0625rem;
+    max-width: 31.25rem;
     width: 100%;
     height: 3.125rem;
     padding: 0.75rem 1.25rem;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: flex-end;
+  }
+  .titleModal {
+    text-align: center;
+    color: #ffffff;
+    font-family: "Roboto", sans-serif;
+    font-weight: 600;
+    font-size: 26px;
+  }
+  .btCloseModal {
+    max-width: 0.75rem;
+    width: 100%;
+    height: 1.5rem;
+    color: rgba(255, 255, 255, 0.5);
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    font-size: 1rem;
+  }
+  .btCloseModal:hover {
+    -webkit-transition: scale(1.1);
+    -webkit-transform: scale(1.1);
+    color: #ffffff;
+  }
+  .divContact {
+    margin-top: 2.75rem;
+    max-width: 31.25rem;
+    width: 100%;
+    padding-left: 0rem 0.625rem 0rem 0.625rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .whatsAppImage {
+    height: 5.625rem;
+  }
+  @media (max-width: 34.375rem) {
+    .modalContent {
+      width: 80%;
+    }
+    .divContact {
+      justify-content: space-evenly;
+    }
+  }
+
+  @keyframes animation {
+    from {
+      opacity: 0;
+      transform: translateY(-70%);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0%);
+    }
   }
 `;
