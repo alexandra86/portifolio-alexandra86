bbc23983fb49ce8e962c34c4edaf98485a0f506d
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-08-16 20:47:05 -0300

    feat: adicionado mais projetos ao arquivo projectsDatabase.js

--- src/utils/projectsDatabase.js
+++ src/utils/projectsDatabase.js
@@ -3,6 +3,9 @@ import hamburgueria from "../img/hamburgueria.png";
 import hamburgueriaDois from "../img/hamburgueria_2_0.png";
 import closeWork from "../img/closeWork.png";
 import bandKamp from "../img/bandkamp.png";
+import listaDeCompras from "../img/listaDeCompras.png";
+import movies from "../img/movies.png";
+import kImovies from "../img/kImoveis.png";
 
 export const projectDataBase = [
   {
@@ -43,5 +46,32 @@ export const projectDataBase = [
     title: "BandKamp",
     repositoryLink: "https://github.com/alexandra86/m5-bandkamp-alexandra86",
     deployLink: "https://web-production-36aae.up.railway.app/api/docs/",
+  },
+
+  {
+    id: 6,
+    img: listaDeCompras,
+    title: "Lista de Compras",
+    repositoryLink:
+      "https://github.com/alexandra86/m4-sp1-lista-de-compras-alexandra86",
+    deployLink:
+      "https://github.com/alexandra86/m4-sp1-lista-de-compras-alexandra86",
+  },
+
+  {
+    id: 7,
+    img: movies,
+    title: "Movies",
+    repositoryLink: "https://github.com/alexandra86/m4-sp2-movies-alexandra86",
+    deployLink: "https://github.com/alexandra86/m4-sp2-movies-alexandra86",
+  },
+
+  {
+    id: 8,
+    img: kImovies,
+    title: "KImóvies",
+    repositoryLink:
+      "https://github.com/alexandra86/m4-t14-kimoveis-alexandra86",
+    deployLink: "https://github.com/alexandra86/m4-t14-kimoveis-alexandra86",
   },
 ];
